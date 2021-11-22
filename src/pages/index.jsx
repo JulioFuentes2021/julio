2976da04ae9d3fcad7775a395a9b51ad29165b71
Julio Fuentes <juliodddddd1234@gmail.com>
2021-11-22 14:56:11 -0600

    My career has been added

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Header from '../components/Header';
 import MenuMobile from '../components/MenuMobile';
+import ImJulio from '../components/ImJulio';
 
 const Index = () => {
     return (
@@ -11,6 +12,7 @@ const Index = () => {
             <div className="">
                 <MenuMobile />
             </div>
+            <ImJulio />
         </div>
     )
 }
