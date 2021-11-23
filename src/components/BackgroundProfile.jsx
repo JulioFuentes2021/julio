0e00e00b0a1f5527a0dda6f3c9ccb8e430c612ee
Julio Fuentes <juliodddddd1234@gmail.com>
2021-11-23 10:47:56 -0600

    Presentation component is working

--- src/components/BackgroundProfile.jsx
+++ src/components/BackgroundProfile.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const BackgroundProfile = () => {
     return (
-        <div className="flex items-center overflow-hidden m-3 w-72 h-72 bg-blue-400 roundedCustom">
+        <div className="flex items-center overflow-hidden m-3 min-w-min w-72 h-72 bg-blue-400 roundedCustom">
             <img src="https://www.thoughtco.com/thmb/nhezwxPKb0WrFp_5ogIuuOh2IhA=/2576x2576/smart/filters:no_upscale()/american-bald-eagle-head-in-front-of-american-flag-950701786-5b85581346e0fb0025e00b06.jpg" alt="" className="w-48 m-auto" />
         </div>
     )
