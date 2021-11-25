fa4e6856f3e9da831376c71e8c7ed438be151960
Julio Fuentes <juliodddddd1234@gmail.com>
2021-11-25 15:34:58 -0600

    Portfolio is working

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
-import App from './components/Presentation';
+import App from './pages/index';
 import { ContextProvider } from './context/index';
 
 ReactDOM.render(
