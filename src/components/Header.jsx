7d92439ec9cf045f84f26ff6428ff68003def7b3
Julio Fuentes <juliodddddd1234@gmail.com>
2021-12-01 15:38:33 -0600

    Dark Mode bug is solved

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,49 +1,38 @@
-import React from 'react'
-import { Context } from '../context';
-import { useContext } from 'react';
-import { FaSun, FaMoon, FaBars } from 'react-icons/fa'
+import React from "react";
+import { Context } from "../context";
+import { useContext } from "react";
+import { FaSun, FaMoon, FaBars } from "react-icons/fa";
 
 const Header = () => {
-    const marginXForLi = 'mx-2'
-    const { darkMode, setDarkMode, manageDarkMode } = useContext(Context);
+	const marginXForLi = "mx-2";
+	const { darkMode, setDarkMode, manageDarkMode } = useContext(Context);
 
-    // const handleDarkMode = () => {
-    //     setDarkMode(!darkMode);
-    // };
+	// const handleDarkMode = () => {
+	//     setDarkMode(!darkMode);
+	// };
 
-    return (
-            <header className={`hidden justify-evenly my-6 darkModeIsApplied md:flex`}>
-                <div className="hoverForHeader">
-                    Fuentes
-                </div>
-                {/* <div className="md:hidden">
+	return (
+		<header className={`hidden justify-evenly darkModeIsApplied md:flex`}>
+			<div className="hoverForHeader">Fuentes</div>
+			{/* <div className="md:hidden">
                     <FaBars />
                 </div> */}
-                <ul className="flex">
-                    <li className={`${marginXForLi} hoverForHeader`}>
-                        Home
-                    </li>
-                    <li className={`${marginXForLi} hoverForHeader`}>
-                        About
-                    </li>
-                    <li className={`${marginXForLi} hoverForHeader`}>
-                        Skills
-                    </li>
-                    <li className={`${marginXForLi} hoverForHeader`}>
-                        Services
-                    </li>
-                    <li className={`${marginXForLi} hoverForHeader`}>
-                        Portfolio
-                    </li>
-                    <li className={`${marginXForLi} hoverForHeader`}>
-                        Contact
-                    </li>
-                    <li className={`${marginXForLi} hoverForHeader m-auto`} onClick={manageDarkMode}>
-                        {!darkMode ? <FaMoon/> : <FaSun/>}
-                    </li>
-                </ul>
-            </header>
-    )
-}
+			<ul className="flex">
+				<li className={`${marginXForLi} hoverForHeader`}>Home</li>
+				<li className={`${marginXForLi} hoverForHeader`}>About</li>
+				<li className={`${marginXForLi} hoverForHeader`}>Skills</li>
+				<li className={`${marginXForLi} hoverForHeader`}>Services</li>
+				<li className={`${marginXForLi} hoverForHeader`}>Portfolio</li>
+				<li className={`${marginXForLi} hoverForHeader`}>Contact</li>
+				<li
+					className={`${marginXForLi} hoverForHeader m-auto`}
+					onClick={manageDarkMode}
+				>
+					{!darkMode ? <FaMoon /> : <FaSun />}
+				</li>
+			</ul>
+		</header>
+	);
+};
 
 export default Header;
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,46 +1,48 @@
-import React from 'react'
-import Header from '../components/Header';
-import MenuMobile from '../components/MenuMobile';
-import ImJulio from '../components/ImJulio';
-import Portfolio from '../components/Portfolio';
-import BackgroundProfile from '../components/BackgroundProfile'
-import SocialNetworks from '../components/SocialNetworks';
+import React from "react";
+import Header from "../components/Header";
+import MenuMobile from "../components/MenuMobile";
+import ImJulio from "../components/ImJulio";
+import Portfolio from "../components/Portfolio";
+import BackgroundProfile from "../components/BackgroundProfile";
+import SocialNetworks from "../components/SocialNetworks";
 
 const Index = () => {
-    return (
-        <div className="darkModeIsApplied h-full">
-            <div className="">
-                <Header />
-            </div>
-            <div className="">
-                <MenuMobile />
-            </div>
-            <div className="flex flex-col items-center sm:flex-row sm: justify-around">
-                <div className="flex items-center">
-                    <SocialNetworks />
-                    <ImJulio />
-                </div>
-                <BackgroundProfile />
-            </div>
-            <div className="portfolioGrid">
-                <Portfolio
-                    image="https://estaticos.muyinteresante.es/media/cache/760x570_thumb/uploads/images/article/570222425cafe8efee31eef0/tigre_0.jpg"
-                    title="CountryInfo"
-                    content="lorem ipsumco conmcosncs imcoismcos incsoicmos"
-                />
-                <Portfolio 
-                image="https://estaticos.muyinteresante.es/media/cache/760x570_thumb/uploads/images/article/570222425cafe8efee31eef0/tigre_0.jpg"
-                title="CountryInfo"
-                content="lorem ipsumco conmcosncs imcoismcos incsoicmos"
-                />
-                <Portfolio 
-                image="https://estaticos.muyinteresante.es/media/cache/760x570_thumb/uploads/images/article/570222425cafe8efee31eef0/tigre_0.jpg"
-                title="CountryInfo"
-                content="lorem ipsumco conmcosncs imcoismcos incsoicmos"
-                />
-            </div>
-        </div>
-    )
-}
+	return (
+		<div className="h-screen darkModeIsApplied">
+			<div className="h-full">
+				<div className="">
+					<Header />
+				</div>
+				<div className="">
+					<MenuMobile />
+				</div>
+				<div className="flex flex-col items-center sm:flex-row sm: justify-around">
+					<div className="flex items-center">
+						<SocialNetworks />
+						<ImJulio />
+					</div>
+					<BackgroundProfile />
+				</div>
+				<div className="portfolioGrid">
+					<Portfolio
+						image="https://estaticos.muyinteresante.es/media/cache/760x570_thumb/uploads/images/article/570222425cafe8efee31eef0/tigre_0.jpg"
+						title="CountryInfo"
+						content="lorem ipsumco conmcosncs imcoismcos incsoicmos"
+					/>
+					<Portfolio
+						image="https://estaticos.muyinteresante.es/media/cache/760x570_thumb/uploads/images/article/570222425cafe8efee31eef0/tigre_0.jpg"
+						title="CountryInfo"
+						content="lorem ipsumco conmcosncs imcoismcos incsoicmos"
+					/>
+					<Portfolio
+						image="https://estaticos.muyinteresante.es/media/cache/760x570_thumb/uploads/images/article/570222425cafe8efee31eef0/tigre_0.jpg"
+						title="CountryInfo"
+						content="lorem ipsumco conmcosncs imcoismcos incsoicmos"
+					/>
+				</div>
+			</div>
+		</div>
+	);
+};
 
 export default Index;
