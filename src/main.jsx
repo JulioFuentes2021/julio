1608bc8215ad95eec5d55656d169727dc4ed3972
Julio Fuentes <juliodddddd1234@gmail.com>
2021-12-01 15:10:58 -0600

    ExtraContect is ready

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import App from './components/ContactForm';
-import { ContextProvider } from './context/index';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./components/ExtraContact";
+import { ContextProvider } from "./context/index";
 
 ReactDOM.render(
-  <ContextProvider>
-    <App />
-  </ContextProvider>,
-  document.getElementById('root')
-)
+	<ContextProvider>
+		<App />
+	</ContextProvider>,
+	document.getElementById("root")
+);
