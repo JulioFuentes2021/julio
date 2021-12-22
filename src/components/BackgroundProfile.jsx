6731912e66f8ac91613a900ea51933291ae2bccf
Julio Fuentes <juliodddddd1234@gmail.com>
2021-12-22 15:22:58 -0600

    Website is ready for deploy

--- src/components/BackgroundProfile.jsx
+++ src/components/BackgroundProfile.jsx
@@ -1,11 +1,12 @@
-import React from 'react'
+import React from "react";
+import Photo from "../images/perfil.jpg";
 
 const BackgroundProfile = () => {
-    return (
-        <div className="flex items-center overflow-hidden m-3 min-w-min w-72 h-72 bg-blue-400 roundedCustom">
-            <img src="https://www.thoughtco.com/thmb/nhezwxPKb0WrFp_5ogIuuOh2IhA=/2576x2576/smart/filters:no_upscale()/american-bald-eagle-head-in-front-of-american-flag-950701786-5b85581346e0fb0025e00b06.jpg" alt="" className="w-48 m-auto" />
-        </div>
-    )
-}
+	return (
+		<div className="flex items-center overflow-hidden m-3 min-w-min w-72 h-72 bg-blue-400 roundedCustom">
+			<img src={Photo} alt="" className="w-72 m-auto" />
+		</div>
+	);
+};
 
-export default BackgroundProfile
+export default BackgroundProfile;
